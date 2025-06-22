05dce5b89721c2c14681189ae7c4ca78e6bd8e29
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-06-22 20:53:23 +0100

    feat: app download section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from '@/components/Hero';
 import JobListing from '@/components/JobListing';
+import AppDownload from '@/components/AppDownload';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <JobListing />
+      <AppDownload />
     </div>
   );
 };
